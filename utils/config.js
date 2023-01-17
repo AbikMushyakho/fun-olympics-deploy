8b33d564a9d3e2f608d80d067fe151b945b45989
AbikMushyakho <abikmushyakho94@gmail.com>
2023-01-17 19:02:39 +0545

    sender mail added in env

--- utils/config.js
+++ utils/config.js
@@ -6,5 +6,16 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const SENDER_EMAIL = process.env.EMAIL;
 const EMAIL_PASSWORD = process.env.PASSWORD;
 const SECRET = process.env.SECRET;
-
-export { MONGODB_URI, PORT, SENDER_EMAIL, EMAIL_PASSWORD,SECRET };
+const EMAIL_HOST = process.env.EMAIL_HOST;
+const EMAIL_PORT = process.env.EMAIL_PORT;
+const FROM_EMAIL = process.env.FROM_EMAIL;
+export {
+  MONGODB_URI,
+  PORT,
+  SENDER_EMAIL,
+  EMAIL_PASSWORD,
+  SECRET,
+  EMAIL_HOST,
+  EMAIL_PORT,
+  FROM_EMAIL,
+};
